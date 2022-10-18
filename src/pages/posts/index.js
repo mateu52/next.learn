95ed2a258aaa7b23010997cf726a481f6c88ce45
mateu52 <wuoelte@gmail.com>
2022-10-18 08:03:26 +0200

    statyczne propsy

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
 
-function Posts() {
-    const [ posts, setPosts ] = useState([]);
+function Posts( { posts }) {
+    //const [ posts, setPosts ] = useState([]);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => setPosts(data.slice(0,10)))
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/posts')
+    //     .then(response => response.json())
+    //     .then(data => setPosts(data.slice(0,10)))
+    // }, [])
     return(
         <div>
             <h1>Posts</h1>
@@ -18,6 +18,17 @@ function Posts() {
             )}
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+
+    return {
+        props: {
+            posts: data.slice(0,10)
+        }
+    }
 }
 
 export default Posts;
