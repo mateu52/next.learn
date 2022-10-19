a58892186fc17caa44625076c0be5edcadb32e8e
mateu52 <wuoelte@gmail.com>
2022-10-19 07:48:30 +0200

    api routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,37 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ offers }) {
+  console.log({offers})
   return (
     <div className={styles.container}>
-
+      <h2>Offers</h2>
       <main className={styles.main}>
-        
+        {offers && offers.map((offer) =>
+          <div key={offer.id} className={styles.offersContainer}>
+              <div className={styles.cover}>
+                <img src={offer.cover} alt='' ></img>
+              </div>
+              <div>
+              <h3>{offer.name}</h3>
+              <p>{offer.street}, {offer.city}</p>
+              </div>
+          </div>
+        )}
       </main>
 
     </div>
   )
+}
+
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3000/api/offers');
+  const data = await response.json();
+
+  return{
+    props: {
+      offers: data
+    }
+  }
 }
